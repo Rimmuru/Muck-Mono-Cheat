451530cfc7656c629879a3d0e10e396d5bf8d3d8
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-07-21 06:09:29 +0100

    improvements
    added controls 
    added spawn random boss
    added godmode toggle
    removed spawn boss on injection

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ClassLibrary1
+{
+    public class Loader
+    {
+
+        public static GameObject obj;
+        public static void Load()
+        {
+            obj = new GameObject();
+            obj.AddComponent<ActualShit>();
+            Object.DontDestroyOnLoad(obj);
+        }
+        public static void Unload() 
+        {
+            Object.Destroy(obj);
+        }
+    }
+
+}
